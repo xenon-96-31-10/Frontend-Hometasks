8671ca899b26269059cf27813bd5ee0c2ca556d9
xenon-96-31-10 <arsen9631@gmail.com>
2015-10-21 17:48:21 +0300

    сделал 4, начал 5

--- Lesson 1/Task 4/task.js
+++ Lesson 1/Task 4/task.js
@@ -1,5 +1,43 @@
 // your code goes here
 var arr = [1, 2, 3, 4, 5, 6, 7];
-function filter(arr, funct){
+function filter(arr, func){
+    var arr0  =[];
+    arr.forEach(function(item){
+        if(func(item)==true){
+            arr0.push(item);
+        }
+    });
+    return arr0;
+};
+function inBetween(a,b){
+    return function(item){
+        if((a<=item)&&(item<=b)){
+            return true;
+        };
+    }
+};
 
+function inArray(arr1){
+    return function(item){
+            arr1.forEach(function(item1) {
+                if (item1 == item) {
+                    return true;
+                }
+            })
+    };
 }
+function sumOf(arr2){
+    var a =0;
+    arr2.forEach(function(item){
+        a+=item;
+    });
+    return a;
+}
+/*alert(filter(arr, function(a) {
+    return a % 2 == 0
+})); // 2,4,6*/
+
+//alert( filter(arr, inBetween(3, 6)) ); // 3,4,5,6
+//alert( filter(arr, inArray([1, 2, 10])) ); // 1,2
+alert( sumOf([1, 2, 10, -13]) ); // 0
+
--- Lesson 1/Task 5/task.js
+++ Lesson 1/Task 5/task.js
@@ -1,1 +1,18 @@
 // your code goes here
+function makeLine() {
+
+    var students = [];
+
+    for (var i = 0; i < 10; i++) {
+        var student = function() { // функция-ученик
+            alert( i ); // кричит свой номер
+        };
+        students.push(student);
+    }
+
+    return students;
+}
+
+var line = makeLine();
+
+line[0](); // ученик кричит 10, а должен 0
